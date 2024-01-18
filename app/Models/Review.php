0ab4590f10c4f1f6c0c824745efd409022e739ed
NancyMile <smithnancy@outlook.com>
2024-01-18 04:57:30 +0000

    update cache if theere is some change on db

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -15,4 +15,14 @@ class Review extends Model
     {
         return $this->belongsTo(Book::class);
     }
+
+    //clear the cache when a change happens
+    protected static function booted()
+    {
+        // when a modification is done to a model, // if you use mass assignment,or you use raw query  it wont be trigger,
+        // in database transaction if wont be trigger if the transaction is rolled back
+        //So if you load the model and then modify.. this may be trigged
+        static::updated(fn(Review $review) =>  cache()->forget('book:'.$review->book_id));
+        static::deleted(fn(Review $review) => cache()->forget('book:'.$review->book_id));
+    }
 }
